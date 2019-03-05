747681ca56134b4368eb2915ed28324ef168c3c2
Chris Breaux and Riley Benson <chris+riley@>
2019-03-05 14:04:33 -0800

    add functions for collision detection

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,9 @@
+var counter = 0;
+
+
+
+
+
 function newProjectile() {
   $('ul').append("<li class='projectile'>ball</li>");
   console.log('pew')
@@ -12,11 +18,33 @@ $(document).ready(function(){
   $('ul').click(function(){
     $('.projectile').addClass('move');
     newProjectile();
-
-
+    counter++;
     console.log('test');
+    console.log(counter);
+
   });
 
+  var $target = $('.target'),
+      target = $target[0],
+      targetRect = target.getBoundingClientRect();
+
+  console.log(targetRect);
+
+  function getPosition() {
+    var $projectile = $('.projectile'),
+    projectile = $projectile[counter-1],
+    ballRect = projectile.getBoundingClientRect();
+    console.log(ballRect);
+    if (targetRect.x < ballRect.x + ballRect.width &&
+      targetRect.x + targetRect.width > ballRect.x &&
+      targetRect.y < ballRect.y + ballRect.height &&
+      targetRect.y + targetRect.height > ballRect.y) {
+        alert("collision");
+      }
+
+  }
+
+var test = setInterval(getPosition, 500);
 
 
 });
